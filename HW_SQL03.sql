998897f827ec6698b7a5ef62fb81dbe7a204f130
ofir532 <38012787+ofir532@users.noreply.github.com>
2022-08-22 00:18:25 +0300

    Update HW_SQL03.sql

--- HW_SQL03.sql
+++ HW_SQL03.sql
@@ -38,63 +38,63 @@ FROM employees
 WHERE departments.department_id IN (80,40)
 --05
 SELECT 
-  employees.first_name 
- ,employees.last_name 
- ,departments.department_name
- ,locations.city 
- ,locations.state_province 
+	 employees.first_name 
+	,employees.last_name 
+	,departments.department_name
+	,locations.city 
+	,locations.state_province 
 FROM employees 
 	INNER JOIN departments ON employees.department_id = departments.department_id 
 	INNER JOIN locations ON departments.location_id = locations.location_id 
 WHERE employees.first_name LIKE '%z%';
 --06
 SELECT 
-  employees.first_name 
- ,employees.last_name 
- ,departments.department_id
- ,departments.department_name
+	 employees.first_name 
+	,employees.last_name 
+	,departments.department_id
+	,departments.department_name
 FROM employees 
-	 RIGHT JOIN departments ON employees.department_id = departments.department_id 
+	RIGHT JOIN departments ON employees.department_id = departments.department_id 
 --07
 SELECT 
-  employees.first_name 
- ,employees.last_name 
- ,employees.salary
+	 employees.first_name 
+	,employees.last_name 
+	,employees.salary
 FROM employees
 WHERE employees.salary > ANY (SELECT employees.salary FROM employees WHERE employees.employee_id = 182)
 --08
 SELECT
-  E.first_name AS employee_name
- ,M.first_name AS manager_name
+	 E.first_name AS employee_name
+	,M.first_name AS manager_name
 FROM employees AS E
 	INNER JOIN employees AS M ON M.employee_id = E.manager_id
 --09
 SELECT 
-  departments.department_name
-  ,locations.city
-  ,locations.state_province
+	 departments.department_name
+	,locations.city
+	,locations.state_province
 FROM departments
 	INNER JOIN locations ON locations.location_id = departments.location_id
 
 --10
 SELECT 
-   employees.first_name
-  ,employees.last_name
-  ,departments.department_id
-  ,departments.department_name
+	 employees.first_name
+	,employees.last_name
+	,departments.department_id
+	,departments.department_name
 FROM departments
 	RIGHT JOIN employees ON employees.department_id = departments.department_id
---11
+--11	
 SELECT 
-  E.first_name AS Employee_Name
- ,M.first_name AS Manager
+	 E.first_name AS Employee_Name
+	,M.first_name AS Manager
 FROM employees AS E
 	 LEFT JOIN employees AS M ON M.employee_id = E.manager_id
 --12
 SELECT 
-  employees.first_name
- ,employees.last_name
- ,employees.department_id
+	 employees.first_name
+	,employees.last_name
+	,employees.department_id
 FROM employees
 WHERE
 --employees.department_id IN(SELECT employees.department_id FROM employees WHERE employees.first_name = 'Taylor' ) - No Taylor in the DB
@@ -102,11 +102,11 @@ WHERE
 
 --13
 SELECT 
-  jobs.job_title
- ,departments.department_name
- ,employees.first_name + ' ' + employees.last_name AS employees_name
- ,job_history.start_date
- ,job_history.end_date
+	 jobs.job_title
+	,departments.department_name
+	,employees.first_name + ' ' + employees.last_name AS employees_name
+	,job_history.start_date
+	,job_history.end_date
 FROM employees
 	INNER JOIN departments ON departments.department_id = employees.department_id
 	INNER JOIN jobs ON jobs.job_id = employees.job_id 
@@ -115,11 +115,11 @@ WHERE job_history.start_date = '1993-01-01'
 AND job_history.end_date BETWEEN '1993-01-01' AND '1997-08-31'
 --14
 SELECT 
-  jobs.job_title
- ,employees.first_name + ' ' + employees.last_name AS employees_name
- ,employees.salary
- ,jobs.max_salary
- ,jobs.max_salary - employees.salary AS salary_difference
+	jobs.job_title
+	,employees.first_name + ' ' + employees.last_name AS employees_name
+	,employees.salary
+	,jobs.max_salary
+	,jobs.max_salary - employees.salary AS salary_difference
 FROM employees
 	 INNER JOIN jobs ON jobs.job_id = employees.job_id
 --15
@@ -171,9 +171,6 @@ SELECT
 	,employees.first_name + ' ' + employees.last_name AS name_of_manager
 FROM employees
 	 INNER JOIN departments ON departments.manager_id = employees.employee_id
-
-
-	 SELECT * FROM employees
 --19
 --20
 --21
