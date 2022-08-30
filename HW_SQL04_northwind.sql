e250b2ff82211839e3751e2faccaffde47054918
ofir532 <38012787+ofir532@users.noreply.github.com>
2022-08-30 23:18:38 +0300

    Add files via upload

---
+++ HW_SQL04_northwind.sql
@@ -0,0 +1,92 @@
+--01
+SELECT 
+	Products.ProductName,
+	Products.QuantityPerUnit
+FROM 
+	Products
+
+--02
+SELECT 
+	Products.ProductID,
+	Products.ProductName
+FROM 
+	Products
+WHERE Products.Discontinued = 0
+
+--03
+SELECT 
+	Products.ProductID,
+	Products.ProductName
+FROM 
+	Products
+WHERE Products.Discontinued = 1
+
+--04
+SELECT 
+	Products.ProductName,
+	Products.UnitPrice
+FROM 
+	Products
+ORDER BY Products.UnitPrice DESC
+
+--05
+SELECT 
+	Products.ProductID,
+	Products.ProductName,
+	Products.UnitPrice
+FROM 
+	Products
+WHERE 
+	Products.UnitPrice < 20
+	AND Products.Discontinued = 0
+ORDER BY Products.UnitPrice DESC
+
+--06
+SELECT 
+	Products.ProductID,
+	Products.ProductName,
+	Products.UnitPrice
+FROM 
+	Products
+WHERE 
+	Products.UnitPrice BETWEEN 15 AND 25
+ORDER BY Products.UnitPrice DESC
+
+--07
+SELECT 
+	Products.ProductName,
+	Products.UnitPrice
+FROM 
+	Products
+WHERE 
+	Products.UnitPrice > (SELECT AVG(Products.UnitPrice) FROM Products)
+ORDER BY Products.UnitPrice
+
+--08
+SELECT TOP 10
+	Products.ProductName,
+	Products.UnitPrice
+FROM 
+	Products
+ORDER BY Products.UnitPrice DESC
+
+--09
+SELECT 	COUNT(*) AS NumOfProducts,
+CASE 
+	WHEN Products.Discontinued = 0 THEN 'False'
+	WHEN Products.Discontinued = 1 THEN 'True' 
+	END AS Discontinued
+FROM 
+	Products
+GROUP BY Products.Discontinued
+
+--10
+SELECT
+	Products.ProductName,
+	Products.UnitsOnOrder,
+	Products.UnitsInStock
+FROM 
+	Products
+WHERE
+	Products.UnitsInStock < Products.UnitsOnOrder
+ORDER BY Products.UnitPrice DESC
